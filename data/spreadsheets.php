3e56e8cd232eb1aa6247e7e05e20cf7370eef70c
Thomas Tursics <thomas@tursics.de>
2020-08-26 19:39:25 +0200

    add spreadsheet file reference

---
+++ data/spreadsheets.php
@@ -0,0 +1,30 @@
+<?php
+	// php function to convert csv to json format
+	// see https://www.kodingmadesimple.com/2016/04/convert-csv-to-json-using-php.html
+	function csvToJson($fname) {
+		// open csv file
+		if (!($fp = fopen($fname, 'r'))) {
+			die("Can't open file...");
+		}
+
+		//read csv headers
+		$key = fgetcsv($fp,"1024",",");
+
+		// parse csv rows into array
+		$json = array();
+			while ($row = fgetcsv($fp,"1024",",")) {
+			$json[] = array_combine($key, $row);
+		}
+
+		// release file handle
+		fclose($fp);
+
+		// encode array to json
+		return json_encode($json);
+	}
+
+	$url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS9ofcwxepeQJyN4kc41K55z-oaXYA9BojZzcxGKdz3JD-2DdLhY_tehuptL01MtiRyUX0KH_mF10zs/pub?output=csv';
+
+	header("Access-Control-Allow-Origin: *");
+	print_r(csvToJson($url));
+?>
